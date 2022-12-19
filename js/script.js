5e53dc3e9c3b0097995925faaaab8c93b1b3d673
BBMiranda <bruno.b.miranda@hotmail.com.br>
2022-12-18 23:59:57 -0300

    Portfólio - 01

---
+++ js/script.js
@@ -0,0 +1,8 @@
+/* =================================== Typing Animation =================================== */
+var typed = new Typed(".typing",{
+  strings:["", "Desenvolvedor Salesforce", "Desenvolvedor Front-End"],
+  typeSpeed:80,
+  BackSpeed:40,
+  loop: true
+})
+
